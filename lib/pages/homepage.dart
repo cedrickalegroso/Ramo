955ab3d4c24b268b7c2e2892bb4bd17676aec2c0
Cedrick Alegroso <personal.dancedrick@gmail.com>
2021-03-31 23:35:27 +0800

    changed homepage to a statefull Widget and added BottomNavbar

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -2,22 +2,87 @@ import 'package:ramo/services/authService.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePageStateful extends StatefulWidget {
+  const HomePageStateful({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  _HomePageStateful createState() => _HomePageStateful();
+}
+
+class _HomePageStateful extends State<HomePageStateful> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Index 0: Home',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 1: Business',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 2: School',
+      style: optionStyle,
+    ),
+    Text(
+      'Index 3: Settings',
+      style: optionStyle,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-            child: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Text("Home"),
-        ElevatedButton(
-          onPressed: () {
-            context.read<AuthService>().signOut();
-          },
-          child: Text("Sign Out"),
-        )
-      ],
-    )));
+      appBar: AppBar(
+        title: const Text(
+          'Jacob',
+          style: TextStyle(
+              fontSize: 20.0, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: Center(
+        child: Column(children: [
+      
+          ElevatedButton(
+            onPressed: () {
+              context.read<AuthService>().signOut();
+            },
+            child: Text("Sign Out"),
+          ),
+          _widgetOptions.elementAt(_selectedIndex)
+        ]),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Business',
+          )
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        onTap: _onItemTapped,
+      ),
+    );
   }
 }
