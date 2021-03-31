fde337fba4ad6aa3cab6a57e65c0e28529079851
unor12 <pasilan.carl@gmail.com>
2021-04-01 05:27:00 +0800

    Update authService.dart

--- lib/services/authService.dart
+++ lib/services/authService.dart
@@ -1,6 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
-
 
 class AuthService {
   final FirebaseAuth _firebaseAuth;
@@ -13,22 +12,56 @@ class AuthService {
     await _firebaseAuth.signOut();
   }
 
-  Future<String> signIn({String email, String password}) async {
+  Future<bool> signIn({String email, String password}) async {
     try {
       await _firebaseAuth.signInWithEmailAndPassword(
           email: email, password: password);
-      return 'userfound'; // not the best way AHHAHA
+      print('userfound');
+      return true; // not the best way AHHAHA
     } on FirebaseAuthException catch (e) {
-      return e.message;
+      if (e.code == 'user-not-found') {
+        //update later with message pop ups or kung ano man da
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+      return false;
     }
   }
 
-  Future<String> signUp({String email, String password}) async {
+  Future<bool> signUp({String email, String password, String fullName}) async {
+    // try {
+    //   await _firebaseAuth.createUserWithEmailAndPassword(
+    //       email: email, password: password);
+    // } catch (e) {
+    //   return e.message;
+    // }
     try {
-      await _firebaseAuth.createUserWithEmailAndPassword(
-          email: email, password: password);
-    } catch (e) {
-      return e.message;
+      await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      print('user signed up successfully');
+      addUser(email: email, fullName: fullName);
+      return true;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+      return false;
     }
+  }
+
+  Future<void> addUser({String email, String fullName}) async {
+    CollectionReference users = FirebaseFirestore.instance.collection('Users');
+    String uid = FirebaseAuth.instance.currentUser.uid;
+    users
+        .add({
+          'full name:': fullName,
+          'email': email,
+          'uid': uid,
+        })
+        .then((value) => print("User Added"))
+        .catchError((error) => print("Failed to add user: $error"));
   }
 }
